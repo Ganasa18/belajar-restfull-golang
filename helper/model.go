4c106fc748676bd9a65b040625b1d89d22bc53c7
Ganasa18 <59557434+Ganasa18@users.noreply.github.com>
2023-04-09 21:24:23 +0700

    update category_service_impl

---
+++ helper/model.go
@@ -0,0 +1,21 @@
+package helper
+
+import (
+	"ganasa18/belajar-golang-restful-api/model/domain"
+	"ganasa18/belajar-golang-restful-api/model/web"
+)
+
+func ToCategoryResponse(category domain.Category) web.CategoryResponse {
+	return web.CategoryResponse{
+		Id:   category.Id,
+		Name: category.Name,
+	}
+}
+
+func ToCategoryResponses(categories []domain.Category) []web.CategoryResponse {
+	var categoryResponse []web.CategoryResponse
+	for _, category := range categories {
+		categoryResponse = append(categoryResponse, ToCategoryResponse(category))
+	}
+	return categoryResponse
+}
